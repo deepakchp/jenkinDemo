38774feec843501d8a2e7308cce357d8422d1d3c
deepakchp <deepakchp@gmail.com>
2023-03-20 09:22:18 +0530

    new project

---
+++ src/main/java/sample/webapp/SampleWebApp.java
@@ -0,0 +1,8 @@
+package main.java.sample.webapp;
+
+public class SampleWebApp {
+
+	public static void main(String args[]) {
+		System.out.println("This is a simple web app");
+	}
+}
---
+++ src/test/java/sample/test/SampleTest.java
@@ -0,0 +1,18 @@
+package test.java.sample.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+		assertEquals(true,true);
+	}
+
+}
